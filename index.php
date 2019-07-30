6f6a854268ee370edcdc4ae8f0a108a8be9dce82
AnthonyMoreau <a82moreau@gmail.com>
2019-07-30 13:41:51 +0200

    Calendar ok

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php 
+function translate($tab, $element){
+
+    foreach($tab as $key => $value){
+        if($element === $key){
+            $element = $value;
+        }
+    }
+    return $element;
+}
+CONST day = [
+    "Monday" => "Lundi",
+    "Tuesday" => "Mardi",
+    "Wednesday" => "Mercredi",
+    "Thursday" => "Jeudi",
+    "Friday" => "Vendredi",
+    "Saturday" => "Samedi", 
+    "Sunday" => "Dimanche"
+];
+CONST month = [
+    "January" => "Janvier",
+    "February" => "février",
+    "March" => "Mars",
+    "April" => "Avril",
+    "May" => "Mai",
+    "June" => "Juin",
+    "July" => "Juillet",
+    "August" => "Août",
+    "September" => "Septembre",
+    "October" => "Octobre",
+    "November" => "Novembre",
+    "December" => "Décembre"
+];
 
 CONST seconds_per_day = 86400;
 
@@ -20,7 +52,6 @@ $lastmonth = getdate(mktime($__H, $__m, $__s, date("m")-1, date("d"),   date("Y"
 
 $year__last__contruct = [];
 $year__next__contruct = [];
-
 $decal__day = $now - seconds_per_day;
 
 while($decal__day > $last_year[0]){
@@ -28,7 +59,6 @@ while($decal__day > $last_year[0]){
         $year__last__contruct []= $date[0];
     $decal__day -= seconds_per_day;    
 }
-
 while($now < $next_year[0]){
     $date = getdate($now);
         $year__next__contruct []= $date[0];
@@ -39,11 +69,17 @@ $year = array_merge(array_reverse($year__last__contruct) , $year__next__contruct
 
 foreach($year as $key){
     $date = getdate($key);
+    
     $week_end = ($date["weekday"] === "Saturday" || $date["weekday"] === "Sunday") ? "color: red" : null;
+    
     $today = $__now;
-    $today__n = ($today["weekday"] === $date["weekday"] AND $today["mday"] === $date["mday"] AND $today["month"] === $date["month"]) ? "Aujourd'hui" : null;
+    $today__n = ($today["weekday"] === $date["weekday"] AND $today["mday"] === $date["mday"] AND $today["month"] === $date["month"]) ? "'Aujourd'hui'" : null;
+    
+    $jour = translate(day, $date["weekday"]);
+    $mois = translate(month, $date["month"]);
+
     ?>
-        <p style="<?= $week_end ?>"><?= $date["weekday"] ?>, <?= $date["mday"] ?>, <?= $date["month"] ?>, <?= $date["year"] ?>, <span style="font-size: 2em; color:green"><?= $today__n ?></span></p>
+        <p style="<?= $week_end ?>"><?= $jour ?>, <?= $date["mday"] ?>, <?= $mois ?>, <?= $date["year"] ?> <span style="font-size: 2em; color:green"><?= $today__n ?></span></p>
 
     <?php
 }
